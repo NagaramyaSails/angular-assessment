eda5d086fdc1949a88b407071a85f80a3817e92e
NagaramyaSails <103425769+NagaramyaSails@users.noreply.github.com>
2023-05-16 20:52:18 +0530

    Video-bookmark

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
+import { LoginComponent } from './routes/login/login.component';
+import { VideosComponent } from './routes/videos/videos.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [{ path: '', component: HomeComponent },
+{path: 'login', component: LoginComponent},
+{path: "videos", component: VideosComponent},
+{path: "**", component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './routes/home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './routes/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { VideosComponent } from './routes/videos/videos.component';
+import { DatePipe } from '@angular/common';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  declarations: [AppComponent, HomeComponent, LoginComponent, VideosComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, HttpClientModule],
+  providers: [DatePipe],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
