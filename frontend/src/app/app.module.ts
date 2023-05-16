b04dd175cbfde7b81773728f453aa8b4494db9e3
NagaramyaSails <103425769+NagaramyaSails@users.noreply.github.com>
2023-05-16 20:53:37 +0530

    Revert "Video-bookmark"
    This reverts commit eda5d086fdc1949a88b407071a85f80a3817e92e.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,16 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './routes/home/home.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './routes/login/login.component';
-import { HttpClientModule } from '@angular/common/http';
-import { VideosComponent } from './routes/videos/videos.component';
-import { DatePipe } from '@angular/common';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, LoginComponent, VideosComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, HttpClientModule],
-  providers: [DatePipe],
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
